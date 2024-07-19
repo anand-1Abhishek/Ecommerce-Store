9cecade833c1c5f634135cf7b97c4bca756dfe82
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-07-19 17:29:17 +0530

    Created specific product page

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -8,18 +8,18 @@ import ProductList from "@/components/product-list";
 export const revalidate = 0;
 
 const HomePage = async () => {
-    const products = await getProducts({isFeatured: true});
+    const products = await getProducts({ isFeatured: true });
     const billboard = await getBillboard("791fcbad-c87b-45bf-a13c-2cabc661a825");
-    return ( 
+    return (
         <Container>
             <div className="space-y-10 pb-10">
-                <Billboard data={billboard}/>
+                <Billboard data={billboard} />
             </div>
-            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
-                <ProductList title="Featured Products" items={products}/>
+            <div className='flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8 '>
+                <ProductList title="Featured Products" items={products} />
             </div>
         </Container>
     );
 }
- 
+
 export default HomePage;

944833de5c3a3e058444454b7a35a856c5f08863
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-07-19 16:12:27 +0530

    Added product card

--- actions/get-categories.tsx
+++ actions/get-categories.tsx
@@ -2,9 +2,10 @@ import { Category } from "@/types";
 
 const URL = `${process.env.NEXT_PUBLIC_API_URL}/categories`;
 
-const getCategories = async () : Promise<Category[]> =>{
+const getCategories =  async (): Promise<Category[]> => {
+    //console.log(URL)
     const res = await fetch(URL);
     return res.json();
-};
-
+}
+ 
 export default getCategories;
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
 
-const HomePage = () => {
-  return (
-    <div>
-      Hello world
-    </div>
-  )
+import getBillboard from "@/actions/get-billboards";
+import getProducts from "@/actions/get-products";
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+    const products = await getProducts({isFeatured: true});
+    const billboard = await getBillboard("791fcbad-c87b-45bf-a13c-2cabc661a825");
+    return ( 
+        <Container>
+            <div className="space-y-10 pb-10">
+                <Billboard data={billboard}/>
+            </div>
+            <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+                <ProductList title="Featured Products" items={products}/>
+            </div>
+        </Container>
+    );
 }
-
-export default HomePage
+ 
+export default HomePage;

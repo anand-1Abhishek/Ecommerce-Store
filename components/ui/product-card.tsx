dc88593f5c3d1e3fc0487c9f4588266c2792285b
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-07-20 12:21:03 +0530

    created cart page

--- components/ui/product-card.tsx
+++ components/ui/product-card.tsx
@@ -1,12 +1,15 @@
-"use client";
-import { Product } from '@/types';
+"use client"
+
 import Image from 'next/image';
-import IconButton from './icon-button';
-import {Expand, ShoppingCart} from "lucide-react"
-import Currency from './currency';
+import {Expand, ShoppingCart} from "lucide-react";
 import { useRouter } from 'next/navigation';
-import React, { MouseEventHandler } from 'react'
+import { MouseEventHandler } from 'react';
+
+import IconButton from './icon-button';
+import { Product } from '@/types';
+import Currency from './currency';
 import usePreviewModal from '@/hooks/use-preview-modal';
+import useCart from '@/hooks/use-cart';
 
 interface ProductCard{
     data: Product,
@@ -16,7 +19,9 @@ const ProductCard: React.FC<ProductCard> = ({
     data
 }) => {
     const previewModal = usePreviewModal();
+    const cart = useCart();
     const router = useRouter();
+
     const handleClick = ()=>{
         router.push(`/product/${data?.id}`);
     }
@@ -27,11 +32,24 @@ const ProductCard: React.FC<ProductCard> = ({
         previewModal.onOpen(data);
     }
 
+    const onAddToCart : MouseEventHandler<HTMLButtonElement> = (event)=>{
+        event.stopPropagation();
+
+        cart.addItem(data);
+    }
+
+
+
     return (
-        <div onClick={handleClick} className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4 '>
+        <div onClick = {handleClick} className='bg-white group cursor-pointer rounded-xl border p-3 space-y-4 '>
             {/* images and Actions */}
             <div className='aspect-square rounded-xl bg-gray-100 relative'>
-                <Image alt='product Image' src={data?.images?.[0]?.url} fill className='aspect-square object-cover rounded-md' />
+                <Image
+                alt='Image'
+                src={data?.images?.[0]?.url}
+                fill
+                className='aspect-square object-cover rounded-md'
+                />
                 <div className='opacity-0 group-hover:opacity-100 transition absolute w-full px-6 bottom-5'>
                     <div className='flex gap-x-6 justify-center'>
                         <IconButton 
@@ -39,7 +57,7 @@ const ProductCard: React.FC<ProductCard> = ({
                             icon={<Expand size={20} className='text-gray-600'/>}
                         />
                         <IconButton 
-                            onClick={()=>{}}
+                            onClick={onAddToCart}
                             icon={<ShoppingCart size={20} className='text-gray-600'/>}
                         />
                     </div>
@@ -47,7 +65,10 @@ const ProductCard: React.FC<ProductCard> = ({
             </div>
             {/* Description */}
             <div>
-                <p className='font-semibold text-lg'>{data.name}</p>
+                <p className='font-semibold text-lg'>
+                    {data.name}
+                </p>
+
                 <p className='text-sm text-gray-500'>
                     {data.category?.name}
                 </p>
@@ -60,4 +81,4 @@ const ProductCard: React.FC<ProductCard> = ({
     )
 }
 
-export default ProductCard
+export default ProductCard;

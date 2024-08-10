41d48591b1e87e9f4330cab71b6953a29df4d25a
Abhishek Anand <120253524+anand-1Abhishek@users.noreply.github.com>
2024-08-10 23:31:19 +0530

    BillboardId changed

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -9,7 +9,7 @@ export const revalidate = 0;
 
 const HomePage = async () => {
     const products = await getProducts({ isFeatured: true });
-    const billboard = await getBillboard("a866b2f8-1ae4-4482-a0a6-28a8130386d4");
+    const billboard = await getBillboard("deac4744-7282-4bfe-b900-1c643bdc83e0");
     return (
         <Container>
             <div className="space-y-10 pb-10">
